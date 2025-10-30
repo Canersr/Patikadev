45f47f536d99bc047aceef13473b409ab444de90
Caner Sürmelioğlu <canersurmelioglu@gmail.com>
2025-10-30 23:20:59 +0300

    Mutlak_Kare_Alma
    Ekrandan girilen n tane sayının 67'den küçük yada büyük olduğuna bakan. Küçük olanların farklarının toplamını, büyük ise de farkların mutlak karelerini alarak toplayıp ekrana yazdıran console uygulaması

---
+++ mutlak kare alma/mutlak kare alma/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace MutlakKareAlma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen n adet tamsayıyı boşlukla giriniz:");
+            string input = Console.ReadLine();
+
+            int[] sayilar = input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int farkToplami = 0;
+            int kareToplami = 0;
+
+            foreach (int sayi in sayilar)
+            {
+                int fark = Math.Abs(sayi - 67);
+
+                if (sayi < 67)
+                {
+                    farkToplami += fark; // Küçük olanların farklarını topla
+                }
+                else if (sayi > 67)
+                {
+                    kareToplami += fark * fark; // Büyük olanların farklarının karelerini topla
+                }
+            }
+
+            Console.WriteLine($"\nOutput: {farkToplami} {kareToplami}");
+        }
+    }
+}
