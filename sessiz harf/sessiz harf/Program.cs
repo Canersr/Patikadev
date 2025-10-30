b31f49c32626388c96ed8e7b45a860aac3bf727c
Caner Sürmelioğlu <canersurmelioglu@gmail.com>
2025-10-30 23:26:28 +0300

    sessiz harf
    Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulaması

---
+++ sessiz harf/sessiz harf/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SessizKontrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen kelimeleri boşlukla giriniz:");
+            string input = Console.ReadLine();
+
+            string[] kelimeler = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string kelime in kelimeler)
+            {
+                bool sonuc = YanYanaIkiSessizVarMi(kelime);
+                Console.Write(sonuc + " ");
+            }
+        }
+
+        static bool YanYanaIkiSessizVarMi(string kelime)
+        {
+            // Türkçedeki sesli harfler
+            string sesliler = "aeıioöuüAEIİOÖUÜ";
+
+            for (int i = 0; i < kelime.Length - 1; i++)
+            {
+                char c1 = kelime[i];
+                char c2 = kelime[i + 1];
+
+                // İki karakter de sessiz mi?
+                if (!sesliler.Contains(c1) && !sesliler.Contains(c2))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
