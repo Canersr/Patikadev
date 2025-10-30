2896d85af608cc63bc1d08b672cfc3002fa5f71a
Caner Sürmelioğlu <canersurmelioglu@gmail.com>
2025-10-30 23:23:45 +0300

    karakter değiştirme
    Verilen string ifade içerisindeki ilk ve son karakterin yerini değiştirip tekrar ekrana yazdıran console uygulaması

---
+++ karakter değiştirme/karakter değiştirme/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace KarakterDegistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen kelimeleri boşlukla giriniz:");
+            string input = Console.ReadLine();
+
+            string[] kelimeler = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] sonuc = kelimeler.Select(KarakterDegistir).ToArray();
+
+            Console.WriteLine("\nOutput:");
+            Console.WriteLine(string.Join(" ", sonuc));
+        }
+
+        static string KarakterDegistir(string kelime)
+        {
+            if (kelime.Length <= 1)
+                return kelime; // tek karakterliyse değiştirme
+
+            char ilk = kelime[0];
+            char son = kelime[kelime.Length - 1];
+
+            // Aradaki kısmı koruyarak ilk ve sonu değiştir
+            string ortasi = kelime.Substring(1, kelime.Length - 2);
+            return son + ortasi + ilk;
+        }
+    }
+}
